7ce3353386a195e4b80b80cf99d4bcd9e9acd0d6
Moe Aydin <maydin@zipcodes-MacBook-Pro-23.local>
2020-02-24 21:55:28 -0500

    passed majority of tests

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,34 +1,55 @@
 package com.zipcodewilmington.phonebook;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBook {
-    public PhoneBook(Map<String, List<String>> map) {
-    }
+
+    Map<String, List<String>> phoneBookMap;
+    List<String> phoneNumbers;
 
     public PhoneBook() {
-        this(null);
+        this.phoneBookMap = new LinkedHashMap<>();
+        this.phoneNumbers = new ArrayList<>();
+    }
+
+    public PhoneBook(Map<String, List<String>> map) {
+        this.phoneBookMap = new LinkedHashMap<>(map.size());
+        this.phoneBookMap.putAll(map);
     }
 
     public void add(String name, String phoneNumber) {
+       this.phoneNumbers.add(phoneNumber);
+       this.phoneBookMap.put(name, this.phoneNumbers);
     }
 
     public void addAll(String name, String... phoneNumbers) {
+        for (int i = 0; i < phoneNumbers.length ; i++) {
+            this.phoneBookMap.put(name, Arrays.asList(phoneNumbers));
+        }
     }
 
     public void remove(String name) {
+        this.phoneBookMap.remove(name);
     }
 
     public Boolean hasEntry(String name) {
-        return null;
+        if(this.phoneBookMap.containsKey(name)){
+            return true;
+        }
+        if(this.phoneBookMap.values().contains(name)){
+            return true;
+        }
+        if(this.phoneNumbers.contains(name)){
+            return true;
+        }
+        return false;
     }
 
     public List<String> lookup(String name) {
-        return null;
+        return this.phoneBookMap.get(name);
     }
 
     public String reverseLookup(String phoneNumber)  {
@@ -40,6 +61,8 @@ public class PhoneBook {
     }
 
     public Map<String, List<String>> getMap() {
-        return null;
+        return this.phoneBookMap;
     }
+
+
 }
